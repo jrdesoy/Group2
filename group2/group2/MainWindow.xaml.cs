5920317a202b5d1e7e3084e149646162bf545154
christinederamas <christinemariederamas@gmail.com>
2022-07-28 11:27:30 +0800

    add refresh and loadgrid

--- group2/group2/MainWindow.xaml.cs
+++ group2/group2/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace group2
     /// </summary>
     public partial class MainWindow : Window
     {
+        string connetionString = "Data Source=LAPTOP-VRFKIHV7;Initial Catalog=db_try;User ID=sa;Password=123";
+
         public MainWindow()
         {
             InitializeComponent();
@@ -51,8 +53,30 @@ namespace group2
 
         }
 
+        public void Loadgrid()
+        {
+            SqlConnection cnn;
+            SqlCommand cmd;
+
+            cnn = new SqlConnection(connetionString);
+
+            cnn.Open();
+            cmd = new SqlCommand("SELECT InfoStudents.StudentID, InfoStudents.Firstname, InfoStudents.Middlename, InfoStudents.Lastname, InfoStudents.Birthdate, COUNT(InfoGuardians.GuardianID) AS 'Number of Guardians' FROM InfoStudents LEFT JOIN InfoGuardians ON InfoStudents.StudentID = InfoGuardians.StudentID GROUP BY InfoStudents.StudentID, InfoStudents.Firstname, InfoStudents.Middlename, InfoStudents.Lastname, InfoStudents.Birthdate", cnn);
+            SqlDataReader sdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            dt.Load(sdr);
+            cmd.Dispose();
+            cnn.Close();
+            datagrid.ItemsSource = dt.DefaultView;
+        }
+
         private void btnRefresh_Click(object sender, RoutedEventArgs e)
         {
+            Loadgrid();
+            txtFirstname.Clear();
+            txtMiddlename.Clear();
+            txtLastname.Clear();
+            dtpBirthdate.Text = string.Empty;
 
         }
 
