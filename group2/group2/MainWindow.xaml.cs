48b146728d9d8671fccfeec9d0a120f7f9422500
Your Name <hdlpuerto@gmail.com>
2022-07-28 11:59:15 +0800

    edit

--- group2/group2/MainWindow.xaml.cs
+++ group2/group2/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace group2
 {
@@ -24,6 +25,15 @@ namespace group2
         {
             InitializeComponent();
         }
+        public static class db_try
+        {
+            public static string conString = "Data Source=LAPTOP-SUP186MI;Initial Catalog = db_try; Integrated Security = True";
+
+            public static SqlConnection GetGlobalConnection()
+            {
+                return new SqlConnection(connectString1);
+            }
+        }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
@@ -32,7 +42,15 @@ namespace group2
 
         private void btnConnect_Click(object sender, RoutedEventArgs e)
         {
-
+            SqlConnection con = new SqlConnection(conString);
+            con.Open();
+            if (con.State==System.Data.ConnectionState.Open)
+            {
+                string q = "INSERT INTO StudentsInfo (StudentID, Firstname, Middlename, Lastname, Birthdate) VALUES ('" + txtFirstname.Text.ToString + "', '" + txtMiddlename.Text.ToString + "', '" + txtLastname.Text.ToString + "', '" + dtpBirthdate.SelectedDate.Value + "')";
+                SqlCommand cmd = new SqlCommand(q, con);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Connection Successful!");
+            }
         }
 
         private void btnRefresh_Click(object sender, RoutedEventArgs e)
